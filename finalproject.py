a1d0173801e7577f8ceedbcda124dc02c4d63186
Ankit Yande <ankityande@gmail.com>
2021-04-29 21:45:13 -0500

    cleaned up

--- finalproject.py
+++ finalproject.py
@@ -2,8 +2,6 @@ import numpy as np
 import cv2 as cv # OpenCV
 # np.set_printoptions(threshold=np.inf)
 
-imgFinal = np.zeros_like(img)
-
 # drawing = False # true if mouse is pressed
 # mode = True # if True, draw rectangle. Press 'm' to toggle to curve
 # ix,iy = -1,-1
@@ -26,57 +24,58 @@ imgFinal = np.zeros_like(img)
 #         drawing=False
 #         cv.line(img,(pt1_x,pt1_y),(x,y),color=(255,0,0),thickness=3)   
 
+def mouse_callback(event,x,y,flags,param):
+    if event== 1:   #cv.EVENT_LBUTTONDOWN
+         print("New seed at:", x,y)
+    elif event== 0: #cv.EVENT_MOUSEMOVE
+          print(x,y)
 
-def line_drawing(event,x,y,flags,param):
+def main():
+    #preprocessing
+    img = cv.imread("./woman.jpg") 
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) #convert to graysacle
+    # img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+    img = cv.GaussianBlur(img,(5,5),cv.BORDER_DEFAULT) #Gaussian Blur the image
 
-    if event== 1: #cv.EVENT_LBUTTONDOWN
+    #edge detection
+    kernelH = np.array([[-1, 0, 1],
+                        [-2, 0, 2],
+                        [-1, 0, 1]]) 
+                    
+    kernelV = np.array([[ 1,  2,  1],
+                        [ 0,  0,  0],
+                        [-1, -2, -1]])
 
+    w,h = img.shape
+    imgFinal = np.zeros_like(img)
+    for y in range(3, h-2):
+        for x in range(3, w-2):
+            localPixels = np.array([[ img[x-1][y-1],  img[x][y-1],  img[x+1][y-1]],
+                                    [ img[x-1][y],    img[x][y],    img[x+1][y]  ],
+                                    [ img[x-1][y+1],  img[x][y+1],  img[x+1][y+1]]])
+            
+            transformPixelsV = kernelV * localPixels
+            scoreV = transformPixelsV.sum()/4
+            
+            transformPixelsH = kernelH * localPixels
+            scoreH = transformPixelsH.sum()/4
+            
+            imgFinal[x][y] = ( scoreV**2 + scoreH**2 )**0.5
 
-#preprocessing
-img = cv.imread("./owl.png") 
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) #convert to graysacle
-# img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+    imgFinal = imgFinal/imgFinal.max()
 
-img = cv.GaussianBlur(img,(5,5),cv.BORDER_DEFAULT) #Gaussian Blur the image
+    #draw image
+    cv.namedWindow('image')
 
-kernelH = np.array([[-1, 0, 1],
-                    [-2, 0, 2],
-                    [-1, 0, 1]]) 
-                   
-kernelV = np.array([[ 1,  2,  1],
-                    [ 0,  0,  0],
-                    [-1, -2, -1]])
+    cv.setMouseCallback('image', mouse_callback)
+    # scissorCoords = list(x, y) ## this is a list of coordinates on the scissor path; starts with mouse x, y
 
-w,h = img.shape
+    while(True):
+        cv.imshow('image',imgFinal)
+        if cv.waitKey(20) & 0xFF == 27:
+            break
 
-imgFinal = np.zeros_like(img)
-for y in range(3, h-2):
-    for x in range(3, w-2):
-        localPixels = np.array([[ img[x-1][y-1],  img[x][y-1],  img[x+1][y-1]],
-                                [ img[x-1][y],    img[x][y],    img[x+1][y]  ],
-                                [ img[x-1][y+1],  img[x][y+1],  img[x+1][y+1]]])
-        
-        transformPixelsV = kernelV * localPixels
-        scoreV = transformPixelsV.sum()/4
-        
-        transformPixelsH = kernelH * localPixels
-        scoreH = transformPixelsH.sum()/4
-        
-        imgFinal[x][y] = ( scoreV**2 + scoreH**2 )**0.5
+    cv.destroyAllWindows()
 
-imgFinal = imgFinal/imgFinal.max()
-# imgFinal = cv.cvtColor(imgFinal, cv.COLOR_GRAY2RGB)
-
-#draw image
-cv.namedWindow('image')
-
-cv.setMouseCallback('image',line_drawing)
-scissorCoords = list(x, y) ## this is a list of coordinates on the scissor path; starts with mouse x, y
-
-while(1):
-    cv.imshow('image',imgFinal)
-
-    if cv.waitKey(20) & 0xFF == 27:
-        break
-
-cv.destroyAllWindows()
+if __name__ == "__main__":
+    main()
