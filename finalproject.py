4ba6b1123c9f5874f1ba545beb7392fb104df0f3
Ankit Yande <ankityande@gmail.com>
2021-05-01 21:10:33 -0500

    less sad dijkstra noises

--- finalproject.py
+++ finalproject.py
@@ -1,149 +1,249 @@
 import numpy as np
 import cv2 as cv # OpenCV
 import queue  
-
-# np.set_printoptions(threshold=np.inf)
-
-# drawing = False # true if mouse is pressed
-# mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-# ix,iy = -1,-1
-# line drawing
-# drawing = False # true if mouse is pressed
-# pt1_x , pt1_y = None , None
-# mouse callback function
-# def line_drawing(event,x,y,flags,param):
-#     global pt1_x,pt1_y,drawing
-
-#     if event== 1: #cv.EVENT_LBUTTONDOWN
-#         drawing=True
-#         pt1_x,pt1_y=x,y
-
-#     elif event== 0: #cv.EVENT_MOUSEMOVE
-#         if drawing==True:
-#             cv.line(img,(pt1_x,pt1_y),(x,y),color=(255,0,0),thickness=3)
-#             pt1_x,pt1_y=x,y
-#     elif event== 4: #cv.EVENT_LBUTTONUP
-#         drawing=False
-#         cv.line(img,(pt1_x,pt1_y),(x,y),color=(255,0,0),thickness=3)   
-
-# def dijkstra(graph, source, dest):  
-#     q = queue.PriorityQueue()
-#     parents = []
-#     distances = []
-#     start_weight = float("inf")
-
-#     for i in graph.get_vertex():
-#         weight = start_weight
-#         if source == i:
-#             weight = 0
-#         distances.append(weight)
-#         parents.append(None)
-
-#     q.put(([0, source]))
-
-#     while not q.empty():
-#         v_tuple = q.get()
-#         v = v_tuple[1]
-
-#         for e in graph.get_edge(v):
-#             candidate_distance = distances[v] + e.weight
-#             if distances[e.vertex] > candidate_distance:
-#                 distances[e.vertex] = candidate_distance
-#                 parents[e.vertex] = v
-#                 q.put(([distances[e.vertex], e.vertex]))
-
-#     shortest_path = []
-#     end = dest
-#     while end is not None:
-#         shortest_path.append(end)
-#         end = parents[end]
-
-#     shortest_path.reverse()
-
-#     return shortest_path, distances[dest]
-
-### 2d array of pixels, startposx, startposy, currmouse_x, currmouse_y
-
-
-def ourDijkstra(imgArr, seed_y, seed_x, mouse_y, mouse_x):
-    # imgArr = imgFinal
-    # print(seed_y, seed_x, mouse_y, mouse_x)
-
-    w,h = imgArr.shape
-    # print(w,h)
-    weightsArr = np.full_like( imgArr, float("-inf"))
-    parentsPath = np.full_like( imgArr, None, dtype=object)
-    q = queue.PriorityQueue()
-
-    weightsArr[seed_x, seed_y] = 0
-    q.put( (seed_x, seed_y) )
-
-    i = 0
-    while not q.empty():
-        currcoords = q.get()
-        curr_x = currcoords[0] 
-        curr_y = currcoords[1]
-        print(curr_x, curr_y)
-        i = i+1
-        if(i == 100):
+from collections import defaultdict
+import sys
+ 
+class Heap():
+ 
+    def __init__(self):
+        self.array = []
+        self.size = 0
+        self.pos = []
+ 
+    def newMinHeapNode(self, v, dist):
+        minHeapNode = [v, dist]
+        return minHeapNode
+ 
+    # A utility function to swap two nodes
+    # of min heap. Needed for min heapify
+    def swapMinHeapNode(self,a, b):
+        t = self.array[a]
+        self.array[a] = self.array[b]
+        self.array[b] = t
+ 
+    # A standard function to heapify at given idx
+    # This function also updates position of nodes
+    # when they are swapped.Position is needed
+    # for decreaseKey()
+    def minHeapify(self, idx):
+        smallest = idx
+        left = 2*idx + 1
+        right = 2*idx + 2
+ 
+        if left < self.size and self.array[left][1] < self.array[smallest][1]:
+            smallest = left
+ 
+        if right < self.size and self.array[right][1] < self.array[smallest][1]:
+            smallest = right
+ 
+        # The nodes to be swapped in min
+        # heap if idx is not smallest
+        if smallest != idx:
+ 
+            # Swap positions
+            self.pos[ self.array[smallest][0]] = idx
+            self.pos[ self.array[idx][0]] = smallest
+ 
+            # Swap nodes
+            self.swapMinHeapNode(smallest, idx)
+ 
+            self.minHeapify(smallest)
+ 
+    # Standard function to extract minimum
+    # node from heap
+    def extractMin(self):
+ 
+        # Return NULL wif heap is empty
+        if self.isEmpty() == True:
             return
+ 
+        # Store the root node
+        root = self.array[0]
+ 
+        # Replace root node with last node
+        lastNode = self.array[self.size - 1]
+        self.array[0] = lastNode
+ 
+        # Update position of last node
+        self.pos[lastNode[0]] = 0
+        self.pos[root[0]] = self.size - 1
+ 
+        # Reduce heap size and heapify root
+        self.size -= 1
+        self.minHeapify(0)
+ 
+        return root
+ 
+    def isEmpty(self):
+        return True if self.size == 0 else False
+ 
+    def decreaseKey(self, v, dist):
+ 
+        # Get the index of v in  heap array
+ 
+        i = int(self.pos[v])
+        # print(i)
+ 
+        # Get the node and update its dist value
+        self.array[i][1] = dist
+ 
+        # Travel up while the complete tree is
+        # not hepified. This is a O(Logn) loop
+        while i > 0 and self.array[i][1] < self.array[(i - 1) // 2][1]:
+ 
+            # Swap this node with its parent
+            self.pos[ self.array[i][0] ] = (i-1)/2
+            self.pos[ self.array[(i-1)//2][0] ] = i
+            self.swapMinHeapNode(i, (i - 1)//2 )
+ 
+            # move to parent index
+            i = (i - 1) // 2
+ 
+    # A utility function to check if a given
+    # vertex 'v' is in min heap or not
+    def isInMinHeap(self, v):
+ 
+        if self.pos[v] < self.size:
+            return True
+        return False
+ 
+ 
+def printArr(dist, n):
+    print("Vertex\tDistance from source")
+    for i in range(n):
+        print ("%d\t\t%d" % (i,dist[i]))
+ 
+ 
+class Graph():
+ 
+    def __init__(self, V):
+        self.V = V
+        self.graph = defaultdict(list)
+ 
+    # Adds an edge to an undirected graph
+    def addEdge(self, src, dest, weight):
+ 
+        # Add an edge from src to dest.  A new node
+        # is added to the adjacency list of src. The
+        # node is added at the beginning. The first
+        # element of the node has the destination
+        # and the second elements has the weight
+        newNode = [dest, weight]
+        self.graph[src].insert(0, newNode)
+ 
+        # Since graph is undirected, add an edge
+        # from dest to src also
+        newNode = [src, weight]
+        self.graph[dest].insert(0, newNode)
+ 
+    # The main function that calulates distances
+    # of shortest paths from src to all vertices.
+    # It is a O(ELogV) function
+    def dijkstra(self, src):
+ 
+        V = self.V  # Get the number of vertices in graph
+        dist = []   # dist values used to pick minimum
+                    # weight edge in cut
+ 
+        # minHeap represents set E
+        minHeap = Heap()
+ 
+        #  Initialize min heap with all vertices.
+        # dist value of all vertices
+        for v in range(V):
+            dist.append(sys.maxsize)
+            minHeap.array.append( minHeap.
+                                newMinHeapNode(v, dist[v]))
+            minHeap.pos.append(v)
+ 
+        # Make dist value of src vertex as 0 so
+        # that it is extracted first
+        minHeap.pos[src] = src
+        dist[src] = 0
+        minHeap.decreaseKey(src, dist[src])
+ 
+        # Initially size of min heap is equal to V
+        minHeap.size = V
+ 
+        # In the following loop,
+        # min heap contains all nodes
+        # whose shortest distance is not yet finalized.
+        while minHeap.isEmpty() == False:
+ 
+            # Extract the vertex
+            # with minimum distance value
+            newHeapNode = minHeap.extractMin()
+            u = newHeapNode[0]
+ 
+            # Traverse through all adjacent vertices of
+            # u (the extracted vertex) and update their
+            # distance values
+            for pCrawl in self.graph[u]:
+ 
+                v = pCrawl[0]
+ 
+                # If shortest distance to v is not finalized
+                # yet, and distance to v through u is less
+                # than its previously calculated distance
+                if minHeap.isInMinHeap(v) and dist[u] != sys.maxsize and pCrawl[1] + dist[u] < dist[v]:
+                    dist[v] = pCrawl[1] + dist[u]
+                    # update distance value
+                    # in min heap also
+                    minHeap.decreaseKey(v, dist[v])
+ 
+        printArr(dist,V)
+ 
+ 
+# Driver program to test the above functions
+# graph = Graph(9)
+# graph.addEdge(0, 1, 4)
+# graph.addEdge(0, 7, 8)
+# graph.addEdge(1, 2, 8)
+# graph.addEdge(1, 7, 11)
+# graph.addEdge(2, 3, 7)
+# graph.addEdge(2, 8, 2)
+# graph.addEdge(2, 5, 4)
+# graph.addEdge(3, 4, 9)
+# graph.addEdge(3, 5, 14)
+# graph.addEdge(4, 5, 10)
+# graph.addEdge(5, 6, 2)
+# graph.addEdge(6, 7, 1)
+# graph.addEdge(6, 8, 6)
+# graph.addEdge(7, 8, 7)
+# graph.dijkstra(0)
 
-        for col in range(curr_y-1, curr_y+2):
-            for row in range(curr_x-1, curr_x+2):
-                ## checking all neighbors of the current pixel
-                if (row < 0 or row >= w or col < 0 or col >= h or (curr_y == col and curr_x == row)): # Check that [row, col] is not out of bounds
-                    continue
-                else:
-                    candidateWeight = weightsArr[curr_x, curr_y] + imgArr[row, col]
-                    print("checking", row, col, " weight:", candidateWeight)
-                    if weightsArr[row, col] < candidateWeight:
-                        weightsArr[row, col] = candidateWeight
-                        parentsPath[row, col] = (curr_x, curr_y)
-                        q.put((row, col))
-                        # print("adding to queue")
-        
-        # print("parent of", parentsPath[curr_x, curr_y], "is", curr_x, curr_y)
-    ##Once we have calculated the costs and determined the shortest path
-    # print(parentsPath)
-    shortest_path = []
-    end = (mouse_x, mouse_y)
-    while end is not None:
-        shortest_path.append(end)
-        end = parentsPath[end[0], end[1]]
-    
-    print(shortest_path)
-    return shortest_path
 
 
-seed_x = seed_y = None
-def mouse_callback(event,x,y,flags,param):
-    if event== 1:   #cv.EVENT_LBUTTONDOWN
-        print("New seed at:", x,y)
-        global seed_x 
-        seed_x = x
-        global seed_y 
-        seed_y = y
-    elif event== 4: #cv.EVENT_MOUSEMOVE
-        print("end at", x,y)
-        mouse_x = x
-        mouse_y = y
-        # cv.circle(imgFinal,(x,y),10,(255,0,0),-1)
-        if(seed_x != None and seed_y != None):
-            path = ourDijkstra(imgFinal, seed_x, seed_y, x, y)
-            # print(len(path))
-            for pixel in path:
-                cv.circle(imgFinal,(pixel[0],pixel[1]),1,(255,0,0),-1)
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ########## preprocessing ##########
 
+print("loading image")
 img = cv.imread("./imgs/pix.png") 
+
+print("preprocessing")
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # convert to graysacle
 # img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
-img = cv.GaussianBlur(img,(5,5),cv.BORDER_DEFAULT) # Gaussian Blur the image
+# img = cv.GaussianBlur(img,(5,5),cv.BORDER_DEFAULT) # Gaussian Blur the image
+print("preprocessing completed")
 
 ######### edge detection ##########
 
+print("edge detection")
 #Horizontal and Vertical Sobel kernels
 kernelH = np.array([[-1, 0, 1],
                     [-2, 0, 2],
@@ -155,7 +255,6 @@ kernelV = np.array([[ 1,  2,  1],
 
 #aply kernels to image
 w,h = img.shape
-print(w,h)
 imgFinal = np.zeros_like(img)
 for y in range(3, h-2):
     for x in range(3, w-2):
@@ -171,18 +270,48 @@ for y in range(3, h-2):
         
         imgFinal[x][y] = ( scoreV**2 + scoreH**2 )**0.5
 
-imgFinal = imgFinal/imgFinal.max()
+imgFinal = imgFinal.max()-imgFinal
+print("edge detection completed")
+
+#### create graph
 
 #draw image
-cv.namedWindow('image')
 
-res = ourDijkstra(imgFinal, 10, 5, 10, 20)
-print(res)
+g = Graph(h*w)
+
+'''
+1 1 1 1
+1 1 0 1
+1 1 0 1
+1 1 1 1
+'''
+
+print("creating graph for scissoring")
+for y in range(h):
+    for x in range(w):
+        
+        for b in range(y-1, y+2):
+            for a in range(x-1, x+2):
+                if (a < 0 or a >= w or b < 0 or b >= h or (y == b and x == a)): # Check that [row, col] is not out of bounds
+                    continue
+                g.addEdge(y*w+x, b*w+a, abs(imgFinal[a][b]))
+print("graph for scissoring completed")
+
+print("performing dijkstra")
+seed = 5 * 24 + 17
+g.dijkstra(seed)
+print("dijkstra complete")
+
+#res = ourDijkstra(img, 18, 5, 18, 20)
+# print(res)
 # cv.setMouseCallback('image', mouse_callback)
 # scissorCoords = list(x, y) ## this is a list of coordinates on the scissor path; starts with mouse x, y
 
+cv.namedWindow('imageOG')
+cv.namedWindow('image')
 while(True):
     cv.imshow('image',imgFinal)
+    cv.imshow('imageOG',img)
     if cv.waitKey(20) & 0xFF == 27:
         break
 
