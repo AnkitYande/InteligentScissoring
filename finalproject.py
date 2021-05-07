6c50ce04c334665f8464cc1182817863cad3fdde
Ankit Yande <ankityande@gmail.com>
2021-05-06 23:31:25 -0500

    bresenham algo moved

--- finalproject.py
+++ finalproject.py
@@ -4,6 +4,7 @@ import enum
 
 from graph import Graph
 from fill import FloodFill
+from bresenham import bresenham
 
 tool = 1
 
@@ -11,90 +12,7 @@ readyToDraw = False
 dijkstraCompleted = False
 allPaths = []
 seed_x = seed_y = None
-
-def bresenham(x1,y1,x2, y2):
-    global img
-    # Iterators, counters required by algorithm
-    # Calculate line deltas
-    dx = x2 - x1
-    dy = y2 - y1
-    # Create a positive copy of deltas (makes iterating easier)
-    dx_abs = abs(dx)
-    dy_abs = abs(dy)
-    # Calculate error intervals for both axis
-    px = 2 * dy_abs - dx_abs
-    py = 2 * dx_abs - dy_abs
-    
-    # The line is X-axis dominant
-    if (dy_abs <= dx_abs): 
-        # Line is drawn left to right
-        if (dx >= 0):
-            x = x1 
-            y = y1 
-            xe = x2
-        else: # Line is drawn right to left (swap ends)
-            x = x2 
-            y = y2 
-            xe = x1
         
-        print(x, y) # Draw first pixel
-        img[y][x] = (255, 0, 255)
-        
-        # Rasterize the line
-        i = 0
-        while (x < xe):
-            x = x + 1
-            # Deal with octants...
-            if (px < 0):
-                px = px + 2 * dy_abs
-            else :
-                if ((dx < 0 and dy < 0) or (dx > 0 and dy > 0)):
-                    y = y + 1
-                else:
-                    y = y - 1
-                px = px + 2 * (dy_abs - dx_abs)
-            # Draw pixel from line span at
-            # currently rasterized position
-            print(x, y)
-            img[y][x] = (255, 0, 255)
-            i += 1
-
-    # The line is Y-axis dominant
-    else: 
-       
-        if (dy >= 0): # Line is drawn bottom to top
-            x = x1 
-            y = y1 
-            ye = y2
-        else: # Line is drawn top to bottom
-            x = x2 
-            y = y2 
-            ye = y1
-
-        print(x, y) # Draw first pixel
-        img[y][x] = (255, 0, 255)
-        
-        # Rasterize the line
-        i = 0
-        while(y < ye):
-            y = y + 1
-            # Deal with octants...
-            if (py <= 0):
-                py = py + 2 * dx_abs
-            else:
-                if ((dx < 0 and dy<0) or (dx > 0 and dy > 0)):
-                    x = x + 1
-                else:
-                    x = x - 1
-                py = py + 2 * (dx_abs - dy_abs)
-
-            # Draw pixel from line span at
-            # currently rasterized position
-            print(x, y)
-            img[y][x] = (255, 0, 255)
-            i += 1
-        
- 
 
 def mouse_callback(event,x,y,flags,param):
     global img
@@ -144,7 +62,7 @@ def mouse_callback(event,x,y,flags,param):
         elif tool == 1:
             if(seed_x != None and seed_y != None):
                 print("running bresenham", seed_x, seed_y, x, y)
-                bresenham(seed_x, seed_y, x, y)
+                bresenham(seed_x, seed_y, x, y, img)
             seed_x = x
             seed_y = y
             
